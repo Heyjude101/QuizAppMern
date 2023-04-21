dc4404a00642621375c10b7f1bb8f6238e9b28d3
heyjude101 <ritikraj2884@gmail.com>
2023-04-21 15:11:09 +0530

    summit 1.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,34 @@
 import '../styles/App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-//import main components
-import Main from './Main';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+/** import components */
+import Main from './Main';
 import Quiz from './Quiz';
 import Result from './Result';
+import { CheckUserExist } from '../helper/helper';
 
 
-//react routes
+/** react routes */
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Main></Main>
+    path : '/',
+    element : <Main></Main>
   },
   {
-    path: '/quiz',
-    element: <Quiz></Quiz>
+    path : '/quiz',
+    element : <CheckUserExist><Quiz /></CheckUserExist>
   },
   {
-    path: '/result',
-    element: <Result></Result>
-  }
+    path : '/result',
+    element : <CheckUserExist><Result /></CheckUserExist>
+  },
 ])
+
 function App() {
   return (
     <>
-    <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </>
   );
 }
